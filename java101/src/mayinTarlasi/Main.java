b3dc46fb9503f9ad2cf6dd8f9fe9bec1ac5ff30e
Abra23 <64593747+Abra23@users.noreply.github.com>
2022-12-08 01:11:07 +0300

    Add files via upload

---
+++ java101/src/mayinTarlasi/Main.java
@@ -0,0 +1,15 @@
+package mayinTarlasi;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        MayinTarlasi mayinTarlasi = new MayinTarlasi(5,5);
+        mayinTarlasi.run();
+
+
+
+    }
+
+}
